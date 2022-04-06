4317be30dc9ad42773098b48f1b65383c454c129
AaronRBetts <aaronrbetts@gmail.com>
2022-04-06 14:34:07 +0300

    added pricing

--- client/src/Components/Testimonials.js
+++ client/src/Components/Testimonials.js
@@ -42,32 +42,34 @@ const Testimonials = () => {
                         
                         <div className={section == 0 ? 'active' : ''}>
                             <div className="img"><img src={headshot1} /></div>
-                            <h2>Lorem P. Ipsum</h2>
-                            <p><i>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</i></p>                    
+                            <h2>Jason Y</h2>
+                            <p><i>Absolutely fantastic service, kind and accommodating. Highly recommended using them </i></p>                    
                         </div>
     
                         <div className={section == 1 ? 'active' : ''}>
                             <div className="img"><img src={headshot2} /></div>
-                            <h2>Mr. Lorem Ipsum</h2>
-                            <p><i>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</i></p>                    
+                            <h2>Kimberley B</h2>
+                            <p><i>They have the BEST service and friendliest team. We would not go anywhere else! All the support for this hardworking family business!</i></p>                    
                         </div>
     
                         <div className={section == 2 ? 'active' : ''}>
                             <div className="img"><img src={headshot3} /></div>
-                            <h2>Lorem Ipsum</h2>
-                            <p><i>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco</i>.</p>                    
+                            <h2>Evie T</h2>
+                            <p><i>These guys!👍👍 Public holiday and my tank runs out. Didn’t even Hesitate. Just advised they’d be a couple of hours!
+I’ve got 4 kiddies in the house, it’s hot and no water would have been unbearable!
+So Grateful for business like this that go above and beyond!</i>.</p>                    
                         </div>
     
                         <div className={section == 3 ? 'active' : ''}>
                             <div className="img"><img src={headshot4} /></div>
-                            <h2>Lorem De Ipsum</h2>
-                            <p><i>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</i></p>                    
+                            <h2>Malindi E</h2>
+                            <p><i>Super helpful, on short notice, after a leak emptied our tank. Very friendly people - highly recommend them to anyone needing to order water.</i></p>                    
                         </div>
     
                         <div className={section == 4 ? 'active' : ''}>
                             <div className="img"><img src={headshot5} /></div>
-                            <h2>Ms. Lorem R. Ipsum</h2>
-                            <p><i>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</i></p>                    
+                            <h2>Quinton W</h2>
+                            <p><i>Been using their service since 2009. Always friendly and prompt service.</i></p>                    
                         </div>
     
                     </div>
